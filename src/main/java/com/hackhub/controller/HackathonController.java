c36a7dac5e1e45d2f54b0d12604c4d2a26eacfe7
AarushGaikwad <Aarushgaikwad789@gmail.com>
2026-06-19 21:49:04 +0530

    indented the code

--- src/main/java/com/hackhub/controller/HackathonController.java
+++ src/main/java/com/hackhub/controller/HackathonController.java
@@ -23,106 +23,56 @@ public class HackathonController {
         this.hackathonService = hackathonService;
     }
 
+    // create hackathon
     @PostMapping("/create")
-    public ResponseEntity<ResponseStatus<HackathonResponseDto>> createHackathon(
-            @RequestBody @Valid HackathonRequestDto request) {
-
-        HackathonResponseDto response =
-                hackathonService.createHackathon(request);
-
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(ResponseStatus.success(response));
+    public ResponseEntity<ResponseStatus<HackathonResponseDto>> createHackathon(@RequestBody @Valid HackathonRequestDto request) {
+        HackathonResponseDto response = hackathonService.createHackathon(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseStatus.success(response));
     }
 
+    // get all hackathons
     @GetMapping("/hackathons")
     public ResponseEntity<ResponseStatus<List<HackathonResponseDto>>> getAllHackathons() {
-
-        List<HackathonResponseDto> response =
-                hackathonService.getAllHackathons();
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(ResponseStatus.success(response));
+        List<HackathonResponseDto> response = hackathonService.getAllHackathons();
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseStatus.success(response));
     }
 
+    // get hackathon by id
     @GetMapping("/{id}")
-    public ResponseEntity<ResponseStatus<HackathonResponseDto>> getHackathonById(
-            @PathVariable Integer id) {
-
-        HackathonResponseDto response =
-                hackathonService.getHackathonById(id);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(ResponseStatus.success(response));
+    public ResponseEntity<ResponseStatus<HackathonResponseDto>> getHackathonById(@PathVariable Integer id) {
+        HackathonResponseDto response = hackathonService.getHackathonById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseStatus.success(response));
     }
 
+    // update hackathon
     @PutMapping("/{id}")
-    public ResponseEntity<ResponseStatus<HackathonResponseDto>> updateHackathon(
-            @PathVariable Integer id,
-            @RequestBody @Valid HackathonRequestDto request) {
-
-        HackathonResponseDto response =
-                hackathonService.updateHackathon(id, request);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(ResponseStatus.success(response));
+    public ResponseEntity<ResponseStatus<HackathonResponseDto>> updateHackathon(@PathVariable Integer id, @RequestBody @Valid HackathonRequestDto request) {
+        HackathonResponseDto response = hackathonService.updateHackathon(id, request);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseStatus.success(response));
     }
 
+    // delete hackathon
     @DeleteMapping("/{id}")
-    public ResponseEntity<ResponseStatus<String>> deleteHackathon(
-            @PathVariable Integer id) {
-
+    public ResponseEntity<ResponseStatus<String>> deleteHackathon(@PathVariable Integer id) {
         hackathonService.deleteHackathon(id);
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(ResponseStatus.success("Hackathon deleted successfully"));
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseStatus.success("Hackathon deleted successfully"));
     }
-    
-    //Organizer API
-    
+
+    // get all hackathons created by the organizer
     @GetMapping("/organizer/{organizerId}")
-    public ResponseEntity<ResponseStatus<List<HackathonResponseDto>>>
-    getHackathonsByOrganizer(
-            @PathVariable Integer organizerId){
-
-        return ResponseEntity.ok(
-                ResponseStatus.success(
-                        hackathonService
-                                .getHackathonsByOrganizer(organizerId)
-                )
-        );
+    public ResponseEntity<ResponseStatus<List<HackathonResponseDto>>> getHackathonsByOrganizer(@PathVariable Integer organizerId) {
+        return ResponseEntity.ok(ResponseStatus.success(hackathonService.getHackathonsByOrganizer(organizerId)));
     }
-    
-    //Organization API
-    
+
+    // get all hackathons created by the organization
     @GetMapping("/organization/{organizationId}")
-    public ResponseEntity<ResponseStatus<List<HackathonResponseDto>>>
-    getHackathonsByOrganization(
-            @PathVariable Integer organizationId){
-
-        return ResponseEntity.ok(
-                ResponseStatus.success(
-                        hackathonService
-                                .getHackathonsByOrganization(organizationId)
-                )
-        );
+    public ResponseEntity<ResponseStatus<List<HackathonResponseDto>>> getHackathonsByOrganization(@PathVariable Integer organizationId) {
+        return ResponseEntity.ok(ResponseStatus.success(hackathonService.getHackathonsByOrganization(organizationId)));
     }
-    
-    //Search API
-    
-    @GetMapping("/search")
-    public ResponseEntity<ResponseStatus<List<HackathonResponseDto>>>
-    searchHackathons(
-            @RequestParam String keyword){
 
-        return ResponseEntity.ok(
-                ResponseStatus.success(
-                        hackathonService.searchHackathons(keyword)
-                )
-        );
+    // search hackathon by name
+    @GetMapping("/search")
+    public ResponseEntity<ResponseStatus<List<HackathonResponseDto>>> searchHackathons(@RequestParam String keyword) {
+        return ResponseEntity.ok(ResponseStatus.success(hackathonService.searchHackathons(keyword)));
     }
 }
--- src/main/java/com/hackhub/controller/UserController.java
+++ src/main/java/com/hackhub/controller/UserController.java
@@ -31,40 +31,29 @@ public class UserController {
 
     // register participant
     @PostMapping("/participant")
-    public ResponseEntity<ResponseStatus<ParticipantResponseDto>> registerParticipant(
-            @RequestBody @Valid ParticipantRequestDto request) {
+    public ResponseEntity<ResponseStatus<ParticipantResponseDto>> registerParticipant(@RequestBody @Valid ParticipantRequestDto request) {
         ParticipantResponseDto response = userService.registerParticipant(request);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(ResponseStatus.success(response));
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseStatus.success(response));
     }
 
     // register organizer
     @PostMapping("/organizer")
-    public ResponseEntity<ResponseStatus<OrganizerResponseDto>> registerOrganizer(
-            @RequestBody @Valid OrganizerRequestDto request) {
+    public ResponseEntity<ResponseStatus<OrganizerResponseDto>> registerOrganizer(@RequestBody @Valid OrganizerRequestDto request) {
         OrganizerResponseDto response = userService.registerOrganizer(request);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(ResponseStatus.success(response));
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseStatus.success(response));
     }
 
     // register judge
     @PostMapping("/judge")
-        public ResponseEntity<ResponseStatus<JudgeResponseDto>> registerJudge(
-            @RequestBody @Valid JudgeRequestDto request) {
+    public ResponseEntity<ResponseStatus<JudgeResponseDto>> registerJudge(@RequestBody @Valid JudgeRequestDto request) {
         JudgeResponseDto response = userService.registerJudge(request);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(ResponseStatus.success(response));
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseStatus.success(response));
     }
 
     // Get all user
     @GetMapping("/users")
     public ResponseEntity<ResponseStatus<List<UserResponseDto>>> getAllUsers() {
         List<UserResponseDto> response = userService.getAllUsers();
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(ResponseStatus.success(response)); //TODO: add this method in the admin controller
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseStatus.success(response)); //TODO: add this method in the admin controller
     }
 }
